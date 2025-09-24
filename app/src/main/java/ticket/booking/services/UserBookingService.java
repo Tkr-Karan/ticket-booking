616292b8a02036fda014766898f1a13d9f53affd
Karan Kumar <karan@probus.io>
2025-09-24 23:56:48 +0530

    add: ticket booking app compeletion with basic functionality.

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,11 +1,104 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.Train;
 import ticket.booking.entities.User;
+import ticket.booking.utils.UserServiceUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     private User user;
 
-    public UserBookingService(User user) {
+    private List<User> userList;
+
+    private final String USER_FILE_PATH = "localDb/users.json";
+
+    public UserBookingService(User user) throws IOException {
         this.user = user;
+        loadUsersInitially();
+    }
+
+    public UserBookingService() throws IOException {
+        loadUsersInitially();
+    }
+
+    public void loadUsersInitially() throws IOException {
+        System.out.println(new File(USER_FILE_PATH) + "FILE Path");
+        userList = objectMapper.readValue(new File(USER_FILE_PATH), new TypeReference<List<User>>() {
+                }
+        );
+
+    }
+
+    public Boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1) {
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (IOException ex) {
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USER_FILE_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBooking() {
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId) {
+        return Boolean.TRUE;
+    }
+
+    public List<Train> getTrains(String source, String destination) {
+        try {
+            TrainService trainService = new TrainService();
+            return trainService.searchTrains(source, destination);
+        } catch (IOException ex) {
+            return new ArrayList<>();
+        }
+    }
+
+    public Boolean bookTrainSeat(Train train, int row, int seat) {
+        try {
+            TrainService trainService = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()) {
+                if (seats.get(row).get(seat) == 0) {
+                    seats.get(row).set(seat, 1);
+                    train.setSeats(seats);
+                    trainService.addTrain(train);
+                    return true; // Booking successful
+                } else {
+                    return false; // Seat is already booked
+                }
+            } else {
+                return false; // Invalid row or seat index
+            }
+        } catch (IOException ex) {
+            return Boolean.FALSE;
+        }
+    }
+
+    public List<List<Integer>> fetchSeats(Train train) {
+        return train.getSeats();
     }
 }
